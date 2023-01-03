2509ab2686582854ddf166e91a433bc97c975f72
Sepehr <SepehrRahimiWor@gmail.com>
2023-01-03 18:20:12 +0330

    create build version

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import MemoryCardGame from "./MemoryCardGame";
-import fruits from "./Data/fruits";
-function App() {
-  return (
-    <MemoryCardGame mainInput={fruits} />
-  );
-}
-
-export default App;
